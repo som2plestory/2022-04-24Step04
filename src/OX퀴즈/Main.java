7ed1115695360ca9f4adb74f49e196669ec40e24
이지희 <som2plestory@naver.com>
2022-04-26 01:36:25 +0900

    2022-04-251

--- src/OX퀴즈/Main.java
+++ src/OX퀴즈/Main.java
@@ -6,11 +6,25 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String[] nArr = new String[sc.nextInt()];
-		String[] anArr = new String[80];
+		int testN = sc.nextInt(); //케이스의 개수
+		char[] anArr = new char[80];
 		
-		for(int i=0; i<nArr.length; i++) {
-			
+		for(int i=0; i<testN; i++) {
+			int score = 0; //점수 초기값
+			int sum = 0; //점수 합계
+			String nArr = sc.next(); // 케이스별 답안
+			for(int j=0; j<nArr.length(); j++) {
+				anArr[j] = nArr.charAt(j); //입력받는 OX문자열을 문자배열에 쪼개 넣기
+			}
+			for(int j=0; j<nArr.length(); j++) {
+				if(anArr[j]=='O') {
+					score++;
+					sum+=score;
+				}else if(anArr[j]=='X') {
+					score = 0;
+				}
+			}
+			System.out.println(sum);
 		}
 		
 		sc.close();
