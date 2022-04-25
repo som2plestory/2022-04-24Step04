a57e2feaf685278d7ae5a5ccc4cb504697d54ba4
이지희 <som2plestory@naver.com>
2022-04-26 00:23:44 +0900

    2022-04-25

--- src/평균은넘겠지/Main.java
+++ src/평균은넘겠지/Main.java
@@ -3,31 +3,32 @@ package 평균은넘겠지;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int sum=0;
-		int mean=0;
-		int overN=0;
-		int[] studentN = new int[sc.nextInt()];
-		for(int i=0; i<studentN.length; i++) {
-			studentN[i] = sc.nextInt();
-			int[] scoreN = new int[studentN[i]];
-			for(int j=0; j<scoreN.length; j++) {
-				scoreN[j] = sc.nextInt();
-				sum+=scoreN[j];
-			}
-			mean = sum/scoreN.length;
-			for(int j=0; j<scoreN.length; j++) {
-				if(scoreN[j]>mean) {
-					overN++;
-				}
-			}
-			System.out.println(overN/studentN[i]);
-		}
-		
-		
-		
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int C = sc.nextInt();//개수 C
+        for(int i =0; i < C; i++){
+            int sum = 0;//합
+            int cnt = 0;//점수넘은 사람 수
+            float avg = 0f;//평균
+            float percent = 0;
+            float studentRatio = 0f;//넘은 학생 비율
+            int N = sc.nextInt();//점수 개수 N
+            int[] arr = new int[N];
+            for(int j = 0; j < N ; j++){
+                arr[j] = sc.nextInt();
+                sum +=arr[j];
+                avg = sum/N;
+            }
+            for(int k = 0; k< arr.length; k++) {
+                if (avg < arr[k]) {//평균보다 점수가 높을경우
+                    cnt++;//카운트를 1올린다.
+                }
+            }
+            studentRatio = (float) cnt / N;
+            percent = studentRatio*100;
+            System.out.println(String.format("%.3f",percent)+ "%");
+            //String.format("%.3f"(반올림할자릿수), percent(출력할 변수)
+        }
+        sc.close();
+    }
 }
---
+++ src/평균은넘겠지/Main_X.java
@@ -0,0 +1,36 @@
+package 평균은넘겠지;
+
+import java.util.Scanner;
+
+public class Main_X {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int C=sc.nextInt();	//C : 반(케이스) 수
+		int sum=0;	//반 학생들의 점수 합계
+		double mean=0;
+		int overN=0;
+		int[] studentN = new int[C]; //(반별 학생수)
+		for(int i=0; i<C; i++) { //반별 계산 
+			studentN[i] = sc.nextInt(); //반 학생수
+			int[] scoreN = new int[studentN[i]]; // (학생별 점수)
+			for(int j=0; j<scoreN.length; j++) { //  
+				scoreN[j] = sc.nextInt(); //학생별 점수
+				sum=sum+scoreN[j]; //학생들 점수 합
+			}
+			mean = (double)sum/studentN[i]; //학생들 평균 점수
+			for(int j=0; j<studentN[i]; j++) { //학생마다 
+				if(scoreN[j]>mean) { //학생별 점수가 평균이상인지
+					overN++; //평균이상인 학생수 체크
+				}
+			}
+	        double studentR = (double)overN/studentN[i]; // 비율 : 평균이상학생수/학생수
+	        double percent = studentR*100; //위 비율을 백분율로 환산
+	        System.out.println(String.format("%.3f",percent)+ "%");
+		}
+		
+		
+		
+		sc.close();
+	}
+}
